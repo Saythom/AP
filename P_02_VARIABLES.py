41f95fa3686f28d3782fe326afb46d951f248a15
Saythom <saythom@chbc.school.nz>
2024-06-24 15:03:40 +1200

    Variables

---
+++ P_02_VARIABLES.py
@@ -0,0 +1,14 @@
+# code to learn about variables...
+
+# Variables can be string (not the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+
+num_string = "5"
+# constants are uppercase
+PI = 3.14
+print(fav_num * 2)
+print(num_string * 5)
+
